c5eef727d6fb30ce0057ee895829a82c5294ec95
andoniskoufris <skoufrisa@gmail.com>
2026-09-09 15:17:21 +1000

    QED: Fixed typo in the zero-potential self-energy formula and cleaned up GreenQED cpp and hpp files

--- src/Modules/GreenQED.cpp
+++ src/Modules/GreenQED.cpp
@@ -7,27 +7,197 @@
 #include "Wavefunction/Wavefunction.hpp"
 #include "fmt/format.hpp"
 #include <complex>
+#include <memory>
 
-DiracSpinor FourierTransformF(const DiracSpinor &F,
-                              std::shared_ptr<const Grid> pGrid);
-
-double p_norm(const DiracSpinor &Fa);
-double p_braket(const DiracSpinor &Fa, const DiracSpinor &Fb);
-
-double rho(const double &p, const double &E);
-double aTerm(const double &p, const double &E);
-double bTerm(const double &p, const double &E);
-
-double aTerm_rho(const double &rho, const double &m);
-double bTerm_rho(const double &rho);
-
-void write_orbitals(const std::string &fname,
-                    const std::vector<DiracSpinor> &orbs);
+namespace Module {
 
 // lambda for sign
 auto sign = [](const double &x) { return x < 0 ? -1.0 : (x > 0 ? 1.0 : 0.0); };
 
-namespace Module {
+double rho(const double &p, const double &E) {
+  // dimensionless combination
+  // rho = (m^2 - p^2) / m^2
+  //     = (m^2 - E^2 + |p|^2) / m^2,
+  // where E = e_v is the physical energy and |p| is the magnitude of the 3-momentum
+  // note: |p| is an integration variable, and is not on the mass-shell
+
+  // to do things in atomic units, E is assumed already to be in atomic units
+  // n.b. m_e = 1 and c = 1/α in a.u.
+  //  - E and m_e * c^2 have the same units; this means that E^2 and m^2 must
+  //    be added together as e_v^2 + 1/α^2
+  //  - E and p * c have the same units (I am letting p be dimensionless); E^2
+  //    and p^2 must be added as E^2 + p^2/α^2
+  // thus, rho = (α^{-4} - E^2 + p^2 * α^{-2})/α^{-4}
+  //           = 1 - α^4 * E^2 + α^2 * p^2
+  // note: to compare to Shabaev should add mc^2 to e_v (since ampsci subtracts
+  //       rest mass from H), so E = e_v + m_e * c^2 = e_v + 1/α^2 (in a.u.)
+
+  const auto alphan2 = 1.0 / (PhysConst::alpha2);
+  const auto alpha4 = PhysConst::alpha2 * PhysConst::alpha2;
+  const auto Enu = E + alphan2;
+  const auto Enu2 = alpha4 * Enu * Enu;
+  const auto pnu = PhysConst::alpha * p;
+
+  return 1.0 - Enu2 + pnu * pnu;
+}
+
+//=============================================================================
+
+DiracSpinor FourierTransformF(const DiracSpinor &F,
+                              std::shared_ptr<const Grid> pGrid) {
+  // initialise Fourier transform to be on momentum space grid
+  DiracSpinor FTransform = DiracSpinor(F.n(), F.kappa(), pGrid);
+
+  const auto grid = F.grid();
+  const auto r = F.grid().r();
+  const auto p = pGrid->r();
+
+  for (auto i = 0ul; i < pGrid->num_points(); i++) {
+    // in atomic units, r is in a.u. in which case what r actually is numerically is r/aB
+    // in atomic units aB = 1, but we want p * r to be dimensionless. This is only the case if we actually use alpha * p
+    const auto p_i = p[i]; // / PhysConst::alpha;
+    const auto s_kappa = sign(F.kappa()) + 0.001;
+
+    for (auto j = F.min_pt(); j < F.max_pt(); j++) {
+      FTransform.f(i) += r[j] * F.f(j) *
+                         SphericalBessel::JL(F.l(), p_i * r[j]) * grid.drdu(j) *
+                         grid.du();
+      FTransform.g(i) += r[j] * F.g(j) *
+                         SphericalBessel::JL(F.l() - int(s_kappa), p_i * r[j]) *
+                         grid.drdu(j) * grid.du();
+    }
+    FTransform.f(i) *= 4 * M_PI;
+    FTransform.g(i) *= 4 * M_PI;
+  }
+
+  return FTransform;
+}
+
+//=============================================================================
+
+double p_norm(const DiracSpinor &Fa) {
+
+  const auto pGrid = &Fa.grid();
+  const auto p = pGrid->r();
+
+  double out = 0.0;
+
+  for (auto i = Fa.min_pt(); i < Fa.max_pt(); i++) {
+    const auto p_i = p[i]; // / PhysConst::alpha;
+
+    out += p_i * p_i * (Fa.f(i) * Fa.f(i) + Fa.g(i) * Fa.g(i)) *
+           pGrid->drdu(i) * pGrid->du();
+  }
+  return out / (8.0 * M_PI * M_PI * M_PI);
+}
+
+//=============================================================================
+
+double p_braket(const DiracSpinor &Fa, const DiracSpinor &Fb) {
+  const auto pGrid = &Fa.grid();
+  const auto p = pGrid->r();
+
+  double out = 0.0;
+
+  const auto [i_min, i_max] = std::pair<std::size_t, std::size_t>{
+    std::max(Fa.min_pt(), Fb.min_pt()), std::min(Fa.max_pt(), Fb.max_pt())};
+
+  if (i_min >= i_max) {
+    return 0.0;
+  }
+
+  for (auto i = i_min; i < i_max; i++) {
+    out += p[i] * p[i] * (Fa.f(i) * Fa.f(i) + Fa.g(i) * Fa.g(i)) *
+           pGrid->drdu(i) * pGrid->du();
+  }
+  return out / (8.0 * M_PI * M_PI * M_PI);
+}
+
+//=============================================================================
+
+double aTerm(const double &rho, const double &m) {
+
+  return 2.0 * m * (1.0 + 2.0 * (rho / (1.0 - rho)) * log(rho));
+}
+
+//=============================================================================
+
+double bTerm(const double &rho) {
+
+  return ((rho - 2.0) / (1.0 - rho)) * (1.0 + (rho / (1.0 - rho)) * log(rho));
+}
+
+//=============================================================================
+void write_orbitals(const std::string &fname,
+                    const std::vector<DiracSpinor> &orbs) {
+  if (orbs.empty())
+    return;
+  const auto &gr = orbs.front().grid();
+
+  std::ofstream of(fname);
+  of << "r ";
+  for (auto &psi : orbs) {
+    // of << "r\'$" << psi.symbol(true) << "$" << "\' ";
+    of << "$" << psi.symbol(true) << "$ ";
+  }
+  of << "\n";
+
+  of << "# f block\n";
+  for (std::size_t i = 0; i < gr.num_points(); i++) {
+    of << gr.r(i) << " ";
+    for (auto &psi : orbs) {
+      of << psi.f(i) << " ";
+    }
+    of << "\n";
+  }
+
+  of << "\n# g block\n";
+  for (std::size_t i = 0; i < gr.num_points(); i++) {
+    of << gr.r(i) << " ";
+    for (auto &psi : orbs) {
+      of << psi.g(i) << " ";
+    }
+    of << "\n";
+  }
+
+  of.close();
+  std::cout << "Orbitals written to file: " << fname << "\n";
+}
+
+double SE_ZeroPotential(const DiracSpinor &F_p, const double &mec2) {
+  const auto ev = F_p.en();
+  const auto pGrid = &F_p.grid();
+  const auto p = pGrid->r();
+
+  const auto f_p = F_p.f();
+  const auto g_p = F_p.g();
+
+  double E = 0.0;
+
+  for (auto i = 0ul; i < pGrid->num_points(); i++) {
+    // to compare to Shabaev, E -> E + m_e * c^2 = E + 1/α^2 (in a.u.)
+    const auto En = ev + (1.0 / PhysConst::alpha2);
+    const auto Rho = rho(p[i], ev);
+    const auto a_rho = aTerm(Rho, mec2);
+    const auto b_rho = bTerm(Rho);
+
+    E += (p[i] * p[i] / PhysConst::alpha2) *
+         (a_rho * (f_p[i] * f_p[i] - g_p[i] * g_p[i]) +
+          b_rho * (En * (f_p[i] * f_p[i] + g_p[i] * g_p[i]) +
+                   2 * sign(F_p.kappa()) * (p[i] / PhysConst::alpha) * f_p[i] *
+                     g_p[i])) *
+         pGrid->drdu(i) * pGrid->du();
+  }
+
+  E *= PhysConst::alpha / (32.0 * pow(M_PI, 4));
+  // fudge factor (probably a units issue) to get agreement with Shabev paper
+  E *= PhysConst::alpha2;
+
+  return E;
+}
+
+//=============================================================================
+//=============================================================================
 
 void GreenQED(const IO::InputBlock &input, const Wavefunction &wf) {
 
@@ -127,7 +297,6 @@ void GreenQED(const IO::InputBlock &input, const Wavefunction &wf) {
   }
 
   //===========================================================================
-  //===========================================================================
   // Calculating self-energy corrections
   std::cout << std::endl;
   std::cout << "Calculating electron self-energy\n";
@@ -180,8 +349,8 @@ void GreenQED(const IO::InputBlock &input, const Wavefunction &wf) {
       // to compare to Shabaev, E -> E + m_e * c^2 = E + 1/α^2 (in a.u.)
       const auto En = ev + (1.0 / PhysConst::alpha2);
       const auto Rho = rho(p[i], ev);
-      const auto a_rho = aTerm_rho(Rho, mec2);
-      const auto b_rho = bTerm_rho(Rho);
+      const auto a_rho = aTerm(Rho, mec2);
+      const auto b_rho = bTerm(Rho);
 
       E += (p[i] * p[i] / PhysConst::alpha2) *
            (a_rho * (vtild.f(i) * vtild.f(i) - vtild.g(i) * vtild.g(i)) +
@@ -208,183 +377,3 @@ void GreenQED(const IO::InputBlock &input, const Wavefunction &wf) {
 }
 
 } // namespace Module
-
-//=============================================================================
-//=============================================================================
-
-double rho(const double &p, const double &E) {
-  // dimensionless combination
-  // rho = (m^2 - p^2) / m^2
-  //     = (m^2 - E^2 + |p|^2) / m^2,
-  // where E = e_v is the physical energy and |p| is the magnitude of the 3-momentum
-  // note: |p| is an integration variable, and is not on the mass-shell
-
-  // to do things in atomic units, E is assumed already to be in atomic units
-  // n.b. m_e = 1 and c = 1/α in a.u.
-  //  - E and m_e * c^2 have the same units; this means that E^2 and m^2 must
-  //    be added together as e_v^2 + 1/α^2
-  //  - E and p * c have the same units (I am letting p be dimensionless); E^2
-  //    and p^2 must be added as E^2 + p^2/α^2
-  // thus, rho = (α^{-4} - E^2 + p^2 * α^{-2})/α^{-4}
-  //           = 1 - α^4 * E^2 + α^2 * p^2
-  // note: to compare to Shabaev should add mc^2 to e_v (since ampsci subtracts
-  //       rest mass from H), so E = e_v + m_e * c^2 = e_v + 1/α^2 (in a.u.)
-
-  const auto alphan2 = 1.0 / (PhysConst::alpha2);
-  const auto alpha4 = PhysConst::alpha2 * PhysConst::alpha2;
-  const auto Enu = E + alphan2;
-  const auto Enu2 = alpha4 * Enu * Enu;
-  const auto pnu = PhysConst::alpha * p;
-
-  return 1.0 - Enu2 + pnu * pnu;
-}
-
-//=============================================================================
-
-DiracSpinor FourierTransformF(const DiracSpinor &F,
-                              std::shared_ptr<const Grid> pGrid) {
-  // initialise Fourier transform to be on momentum space grid
-  DiracSpinor FTransform = DiracSpinor(F.n(), F.kappa(), pGrid);
-
-  const auto grid = F.grid();
-  const auto r = F.grid().r();
-  const auto p = pGrid->r();
-
-  for (auto i = 0ul; i < pGrid->num_points(); i++) {
-    // in atomic units, r is in a.u. in which case what r actually is numerically is r/aB
-    // in atomic units aB = 1, but we want p * r to be dimensionless. This is only the case if we actually use alpha * p
-    const auto p_i = p[i]; // / PhysConst::alpha;
-    const auto s_kappa = sign(F.kappa()) + 0.001;
-
-    for (auto j = F.min_pt(); j < F.max_pt(); j++) {
-      FTransform.f(i) += r[j] * F.f(j) *
-                         SphericalBessel::JL(F.l(), p_i * r[j]) * grid.drdu(j) *
-                         grid.du();
-      FTransform.g(i) += r[j] * F.g(j) *
-                         SphericalBessel::JL(F.l() - int(s_kappa), p_i * r[j]) *
-                         grid.drdu(j) * grid.du();
-    }
-    FTransform.f(i) *= 4 * M_PI;
-    FTransform.g(i) *= 4 * M_PI;
-  }
-
-  return FTransform;
-}
-
-//=============================================================================
-
-double p_norm(const DiracSpinor &Fa) {
-
-  const auto pGrid = &Fa.grid();
-  const auto p = pGrid->r();
-
-  double out = 0.0;
-
-  for (auto i = Fa.min_pt(); i < Fa.max_pt(); i++) {
-    const auto p_i = p[i]; // / PhysConst::alpha;
-
-    out += p_i * p_i * (Fa.f(i) * Fa.f(i) + Fa.g(i) * Fa.g(i)) *
-           pGrid->drdu(i) * pGrid->du();
-  }
-  return out / (8.0 * M_PI * M_PI * M_PI);
-}
-
-//=============================================================================
-
-double p_braket(const DiracSpinor &Fa, const DiracSpinor &Fb) {
-  const auto pGrid = &Fa.grid();
-  const auto p = pGrid->r();
-
-  double out = 0.0;
-
-  const auto [i_min, i_max] = std::pair<std::size_t, std::size_t>{
-    std::max(Fa.min_pt(), Fb.min_pt()), std::min(Fa.max_pt(), Fb.max_pt())};
-
-  if (i_min >= i_max) {
-    return 0.0;
-  }
-
-  for (auto i = i_min; i < i_max; i++) {
-    out += p[i] * p[i] * (Fa.f(i) * Fa.f(i) + Fa.g(i) * Fa.g(i)) *
-           pGrid->drdu(i) * pGrid->du();
-  }
-  return out / (8.0 * M_PI_2 * M_PI);
-}
-
-//=============================================================================
-
-double aTerm(const double &p, const double &E) {
-  // put E and p into same units and then add back rest mass energy
-  const auto alphan2 = 1.0 / (PhysConst::alpha2);
-  const auto Enu = PhysConst::alpha2 * (E + alphan2);
-  const auto pnu = PhysConst::alpha * p;
-
-  return 2.0 *
-         (1.0 + 2.0 *
-                  ((1.0 - Enu * Enu + pnu * pnu) / (Enu * Enu - pnu * pnu)) *
-                  log(1.0 - Enu * Enu + pnu * pnu));
-}
-
-//=============================================================================
-
-double bTerm(const double &p, const double &E) {
-  // put E and p into same units and then add back rest mass energy
-  const auto alphan2 = 1.0 / (PhysConst::alpha2);
-  const auto Enu = PhysConst::alpha2 * (E + alphan2);
-  const auto pnu = PhysConst::alpha * p;
-
-  return ((-1.0 - Enu * Enu + pnu * pnu) / (Enu * Enu - pnu * pnu)) *
-         (1.0 + ((1.0 - Enu * Enu + pnu * pnu) / (Enu * Enu - pnu * pnu)) *
-                  log(1.0 - Enu * Enu + pnu * pnu));
-}
-
-//=============================================================================
-
-double aTerm_rho(const double &rho, const double &m) {
-
-  return 2.0 * m * (1.0 + 2.0 * (rho / (1.0 - rho)) * log(rho));
-}
-
-//=============================================================================
-
-double bTerm_rho(const double &rho) {
-
-  return ((rho - 2.0) / (1.0 - rho)) * (1.0 + (rho / (1.0 - rho)) * log(rho));
-}
-
-//=============================================================================
-void write_orbitals(const std::string &fname,
-                    const std::vector<DiracSpinor> &orbs) {
-  if (orbs.empty())
-    return;
-  const auto &gr = orbs.front().grid();
-
-  std::ofstream of(fname);
-  of << "r ";
-  for (auto &psi : orbs) {
-    // of << "r\'$" << psi.symbol(true) << "$" << "\' ";
-    of << "$" << psi.symbol(true) << "$ ";
-  }
-  of << "\n";
-
-  of << "# f block\n";
-  for (std::size_t i = 0; i < gr.num_points(); i++) {
-    of << gr.r(i) << " ";
-    for (auto &psi : orbs) {
-      of << psi.f(i) << " ";
-    }
-    of << "\n";
-  }
-
-  of << "\n# g block\n";
-  for (std::size_t i = 0; i < gr.num_points(); i++) {
-    of << gr.r(i) << " ";
-    for (auto &psi : orbs) {
-      of << psi.g(i) << " ";
-    }
-    of << "\n";
-  }
-
-  of.close();
-  std::cout << "Orbitals written to file: " << fname << "\n";
-}
--- src/Modules/GreenQED.hpp
+++ src/Modules/GreenQED.hpp
@@ -1,12 +1,32 @@
 #pragma once
+#include <memory>
+#include <vector>
 
 // Forward declare classes:
 class Wavefunction;
+class DiracSpinor;
+class Grid;
 namespace IO {
 class InputBlock;
 }
 
 namespace Module {
+
+DiracSpinor FourierTransformF(const DiracSpinor &F,
+                              std::shared_ptr<const Grid> pGrid);
+
+double p_norm(const DiracSpinor &Fa);
+double p_braket(const DiracSpinor &Fa, const DiracSpinor &Fb);
+
+double rho(const double &p, const double &E);
+
+double aTerm(const double &rho, const double &m);
+double bTerm(const double &rho);
+
+double SE_ZeroPotential(const DiracSpinor &F_p, const double &mec2);
+
+void write_orbitals(const std::string &fname,
+                    const std::vector<DiracSpinor> &orbs);
 
 void GreenQED(const IO::InputBlock &input, const Wavefunction &wf);
 
